af8635344756452a9ada9274804b5a9b73a3f55b
Jasim-hub <jasimr796@gmail.com>
2026-07-09 15:49:21 +0530

    profile api update

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gym-app-66of.onrender.com/api/",
+  baseURL: "https://gym-app-66of.onrender.com/api",
 });
 
 export default API;
